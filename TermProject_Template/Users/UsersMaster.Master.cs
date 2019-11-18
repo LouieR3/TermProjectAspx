111e62b23c0fe35cfca1e5eb887473a40f852d76
unknown <16grodgers@gmail.com>
2019-11-18 08:47:38 -0500

    Update Aspx

---
+++ TermProject_Template/Users/UsersMaster.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TermProject_Template.Users
+{
+    public partial class UsersMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
