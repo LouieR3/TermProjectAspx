6a29d1ce2e151383242ff44185d9340e41dbb456
Louis Rodriguez <lou3@lourodriguez.com>
2019-11-26 14:44:43 -0500

    fixed MerchReg

--- TermProject_Template/Merchant/MerchantRegristration.aspx.cs
+++ TermProject_Template/Merchant/MerchantRegristration.aspx.cs
@@ -11,7 +11,7 @@ using System.Net;
 using System.Collections;
 namespace TermProject_Template
 {
- 
+
     public partial class MerchantRegristration : System.Web.UI.Page
     {
         Validation validate = new Validation();
@@ -24,23 +24,40 @@ namespace TermProject_Template
         protected void btnEnterMerchant_Click(object sender, EventArgs e)
         {
             int count = validate.CheckMerchantExists(txtContactEmail.Text);
-            if(count == 1)
+            if (txtContactEmail.Text == "" || txtMerchantsName.Text == "")
             {
-                String url = "http://cis-iis2.temple.edu/Fall2019/CIS3342_tug45415/Project4API/CreateMerchant/" 
-                + txtMerchantsName.Text + "/" + txtContactEmail.Text;
-                // Create an HTTP Web Request and get the HTTP Web Response from the server.
-                WebRequest request = WebRequest.Create(url);
-                WebResponse response = request.GetResponse();
-                // Read the data from the Web Response, which requires working with streams.
-                Stream theDataStream = response.GetResponseStream();
-                StreamReader reader = new StreamReader(theDataStream);
-                String data = reader.ReadToEnd();
-                reader.Close();
-                response.Close();
-                // Deserialize a JSON string into a double.
-                JavaScriptSerializer js = new JavaScriptSerializer();
-                string result = js.Deserialize<string>(data);
-                lblMessage.Text = result;
+                Response.Write(@"<script langauge='text/javascript'>alert
+                ('Please fill out both fields and try again');</script>");
+                return;
+            }
+            else
+            {
+                if (count == 1)
+                {
+                    String url = "http://cis-iis2.temple.edu/Fall2019/CIS3342_tug45415/Project4API/CreateMerchant/"
+                    + txtMerchantsName.Text + "/" + txtContactEmail.Text;
+                    // Create an HTTP Web Request and get the HTTP Web Response from the server.
+                    WebRequest request = WebRequest.Create(url);
+                    WebResponse response = request.GetResponse();
+                    // Read the data from the Web Response, which requires working with streams.
+                    Stream theDataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(theDataStream);
+                    String data = reader.ReadToEnd();
+                    reader.Close();
+                    response.Close();
+                    // Deserialize a JSON string into a double.
+                    JavaScriptSerializer js = new JavaScriptSerializer();
+                    string result = js.Deserialize<string>(data);
+                    Response.Write(@"<script langauge='text/javascript'>alert
+                ('" + result + "');</script>");
+                    return;
+                }
+                else
+                {
+                    Response.Write(@"<script langauge='text/javascript'>alert
+                    ('This merchant info is incorrect, try again');</script>");
+                    return;
+                }
             }
         }
     }
